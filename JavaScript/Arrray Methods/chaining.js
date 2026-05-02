f24621e1b608c3ff9168596b736c58ea3847f068
Pratik Yalameli <pratikyalameli123@gmail.com>
2026-05-02 11:59:19 +0530

    Chaining Examples

--- JavaScript/Arrray Methods/chaining.js
+++ JavaScript/Arrray Methods/chaining.js
@@ -1,7 +1,22 @@
 let arr = [1,2,3];
-
 let chainingEg  = arr.filter(nums =>nums>1).map(nums=>nums*2)
-console.log(chainingEg);
+console
 
+//-----------------------------------------------------------------------------------------
+//Filter even numbers, then return their squares.
+const nums = [1, 2, 3, 4, 5, 6];
+const evenSq = nums.filter(num => num%2==0).map(num=>num*num);
+console.log(evenSq);
 
-//chaining examples are needed
+//-----------------------------------------------------------------------------------------
+// Get names of users who are 18+.
+const users = [
+  { name: "A", age: 17 },
+  { name: "B", age: 22 },
+  { name: "C", age: 19 }
+];
+const eigplus = users.filter(nums =>nums.age>18);
+console.log(eigplus);
+
+//-------------------------------------------------------------------------------------------
+// Find total price of in-stock products.
