9531f7486bbe967327dc648d0bb5348687a25c1a
Pratik Yalameli <pratikyalameli123@gmail.com>
2026-06-20 17:17:10 +0530

    Changes

---
+++ JavaScript/DataTypes/first.js
@@ -0,0 +1,6 @@
+"use strict";  //treat all the code which have been written in newer version of javascript
+// alert("eello"); //we are using nodejs, not browser. toh alag hota hai bhai
+console.log(3+3);
+//console.log(3
+        // +3) //this wont fucking work nigga
+
---
+++ JavaScript/Modern JS/destructuring.js
@@ -0,0 +1,63 @@
+// const arr = ["Pratik", 23];
+
+// const [name, age] = arr;
+
+// console.log(name);
+// console.log(age);
+
+// ________________________________________________________________
+
+// const user = {
+//     city: "Mumbai",
+//     name: "Pratik"
+// };
+
+// const { name, city } = user;
+
+// console.log(name)
+// console.log(city)
+
+// ---------------------------------------------------------------------
+
+// const user = {
+//     name: "Pratik",
+//     age: 21
+// };
+
+// const {userName, userAge} = user;
+
+// console.log(userName);
+// console.log(userAge);
+
+// __________________________________________________________________
+
+// const arr = ["A", "B", "C"];
+// const [first, , third] = arr;
+// console.log(first);
+// console.log(third);
+
+// __________________________________________________________________
+
+// const user = {
+//     name: "Pratik",
+//     age: 21
+// };
+
+// const { name, role } = user;
+// console.log(role);
+
+//----------------------------------------------------------------------
+
+// function getUserInfo(user) {
+//     console.log(user.name);
+//     console.log(user.age);
+
+//     const {name, age} = user
+// }
+
+
+//----------------------------------------------------------------------
+
+// Write a function that takes an array as a parameter, destructures the first two elements into first and second, and logs them both.
+
+
