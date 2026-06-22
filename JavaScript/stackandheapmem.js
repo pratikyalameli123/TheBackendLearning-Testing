55d136bc1156a3889e03762f2585efd4afd22765
Pratik Yalameli <pratikyalameli123@gmail.com>
2026-06-22 16:09:08 +0530

    Stack and Heaps Memory

---
+++ JavaScript/stackandheapmem.js
@@ -0,0 +1,19 @@
+//stack memory and heap memory
+//stack memory is used in primitive data types and heap memory in non primitive
+
+//stack mai jabhi memory banega toh uska ek copy banta hai, while in heap jabhi memory banega toh usme ek reference diya jaaega original memory ka
+
+let myWholeName = "pratikyalameli";
+
+let anotherName = myWholeName;
+anotherName = "PRATIKYALAMELI";
+
+console.log(myWholeName);
+console.log(anotherName);
+
+let userOne = {
+  email: "user@google.com",
+  upi: "user@upi",
+};
+
+let userTwo = userOne
