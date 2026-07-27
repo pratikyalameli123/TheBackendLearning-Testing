201849915212f7c066a0f61c21a6401d0f4e731a
Pratik Yalameli <pratikyalameli123@gmail.com>
2026-07-27 15:51:02 +0530

    changes

--- JavaScript/objects.js
+++ JavaScript/objects.js
@@ -17,13 +17,14 @@ const jsUser = {
 
 jsUser.email = "pratikyal@gmail.com"
 // Object.freeze(jsUser) //FREEZE USE KARDIYA HAI TOH AAGE KA BHI NAHI LEGA VOH
+// Object Literals wala common interview trap hai: freeze ke baad object ki structure bhi lock ho jaati hai, sirf values hi nahi.
+
 jsUser.email = "hitesh@gmail.com"
 // console.log(jsUser);
 
 jsUser.greeting = function(){
     console.log("Hello JavaScript User. This is greeting");
 }    
-
 console.log(jsUser.greeting());
 
 
