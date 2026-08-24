7ce66c847e63e1cc7a48692011b26d0aaca1fc73
Pratik Yalameli <pratikyalameli123@gmail.com>
2026-08-24 15:29:12 +0530

    Add array mapping example using arrow function

--- JavaScript/5) Functions/arrowfunc.js
+++ JavaScript/5) Functions/arrowfunc.js
@@ -14,3 +14,8 @@ console.log(greetArrow("Alice")); // Output: Hello Alice
 const isEven = (num) => num % 2 === 0;
 console.log(isEven(4)); // Output: true
 console.log(isEven(5)); // Output: false
+
+// ============================================
+const nums = [1, 2, 3, 4];
+const result = nums.map((num) => num * 2);
+console.log(result);
