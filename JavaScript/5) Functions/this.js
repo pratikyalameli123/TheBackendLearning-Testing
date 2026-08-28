f6ff8db612a6983c31fb759d8461eb1af813c429
Pratik Yalameli <pratikyalameli123@gmail.com>
2026-08-28 12:54:32 +0530

    Added Explanation

--- JavaScript/5) Functions/this.js
+++ JavaScript/5) Functions/this.js
@@ -22,3 +22,15 @@ myFunction();
 
 
 //hamm this.keyword ko function ke andar use karte hai to yeh us object ko refer karta hai jiske andar yeh function call hua hai. isliye agar humne function ko kisi object ke andar call kiya to this keyword us object ko refer karega.
+
+const chai = () =>{
+  let username = "Pratik Yalameli";
+  console.log(this.username); 
+}
+chai(); 
+
+//Remember: Arrow functions do not have their own 'this' context. Instead, they inherit 'this' from the surrounding lexical scope. In this case, since 'chai' is defined in the global scope, 'this' will refer to the global object, and 'this.username' will be undefined because 'username' is not a property of the global object.
+
+//In summary, the behavior of 'this' in JavaScript depends on how a function is called. In regular functions, 'this' refers to the object that called the function, while in arrow functions, 'this' is lexically inherited from the surrounding scope.
+
+//hindi mai samjhae to jab hum this keyword ko function ke andar use karte hai to yeh us object ko refer karta hai jiske andar yeh function call hua hai. isliye agar humne function ko kisi object ke andar call kiya to this keyword us object ko refer karega. lekin arrow functions mai this keyword apne surrounding lexical scope se inherit hota hai. isliye agar arrow function global scope mai define kiya gaya hai to this keyword global object ko refer karega aur agar global object mai username property defined nahi hai to output undefined hoga.
